bc8ee26a54288b5b749dee2a9eae91cd968f29f4
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-08 20:15:46 -0400

    recipe Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('Baboosh', 'The best laboosh', 'https://bit.ly/2MM8axl'),
-    new Recipe('Waboosh', 'okay i have no clue', 'https://bit.ly/2PwMdPC')
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   displayRecipe(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,16 @@
 
+import { Recipe } from './recipe.model';
 
 export class RecipeService {
 
+    private recipes: Recipe[] = [
+        new Recipe('Baboosh', 'The best laboosh', 'https://bit.ly/2MM8axl'),
+        new Recipe('Waboosh', 'okay i have no clue', 'https://bit.ly/2PwMdPC')
+    ];
+
+    getRecipes() {
+        return this.recipes.slice(); // return a copy
+    }
     
+
 }
