15c6e0e50c5c25fee2cb1a0a31971c4bf0104a1e
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-08 21:22:52 -0400

    add ing to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
@@ -7,8 +8,16 @@ export class RecipeService {
     public recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('Baboosh', 'The best laboosh', 'https://bit.ly/2MM8axl'),
-        new Recipe('Waboosh', 'okay i have no clue', 'https://bit.ly/2PwMdPC')
+        new Recipe('Baboosh', 
+            'The best laboosh', 
+            'https://bit.ly/2MM8axl', 
+            [new Ingredient('Meat', 1), new Ingredient('Beans', 20)]
+        ),
+        new Recipe('Waboosh', 
+            'okay i have no clue', 
+            'https://bit.ly/2PwMdPC', 
+            [new Ingredient('Chicken', 5), new Ingredient('Tomatoes', 3)]
+        )
     ];
 
     getRecipes() {
